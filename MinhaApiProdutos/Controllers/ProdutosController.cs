5dd2fc9b7b23d3873069063b1e35c7f5577980f8
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-01 18:47:04 -0300

    adicionar suporte ao OpenAPI e Scalar para documentação

--- MinhaApiProdutos/Controllers/ProdutosController.cs
+++ MinhaApiProdutos/Controllers/ProdutosController.cs
@@ -18,4 +18,22 @@ public class ProdutosController : ControllerBase
     }
 
     // Primeiro Metodo: GET /api/produtos
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+    {
+        return await _context.Produtos.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Produto>> GetProduto(int id)
+    {
+        var produto = await _context.Produtos.FindAsync(id);
+
+        if (produto == null)
+        {
+            return NotFound();
+        }
+
+        return produto;
+    }
 }
--- MinhaApiProdutos/Program.cs
+++ MinhaApiProdutos/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using MinhaApiProdutos.Data;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -12,12 +14,14 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
+builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.MapOpenApi();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
