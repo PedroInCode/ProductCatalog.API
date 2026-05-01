335e35b782f7b6c0874f286e8fccdb64aca9a635
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-01 17:22:18 -0300

    feat: criando o esqueleto do ProdutosController

---
+++ MinhaApiProdutos/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using MinhaApiProdutos.Data;
+using MinhaApiProdutos.Models;
+
+namespace MinhaApiProdutos.Controllers;
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+public class ProdutosController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProdutosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // Primeiro Metodo: GET /api/produtos
+}
