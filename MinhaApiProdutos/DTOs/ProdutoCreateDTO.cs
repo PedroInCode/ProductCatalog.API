980c434c0cb433f684acc732c289b4cdb2427e23
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-08 21:20:29 -0300

    adiciona validações de dados (data annotations) no ProdutoCreateDTO

--- MinhaApiProdutos/DTOs/ProdutoCreateDTO.cs
+++ MinhaApiProdutos/DTOs/ProdutoCreateDTO.cs
@@ -1,9 +1,21 @@
-﻿namespace MinhaApiProdutos.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinhaApiProdutos.DTOs;
 
 public class ProdutoCreateDTO
 {
+
+
+    [Required(ErrorMessage = "O nome do produto é obrigatório.")] 
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do produto deve ter entre 3 e 100 caracteres.")]
     public string Nome { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "A descrição do produto é obrigatória.")]
     public string Descricao { get; set; } = string.Empty;
+
+    [Range(0.01, 999999, ErrorMessage = "O preço deve ser um valor positivo.")]
     public decimal Preco { get; set; }
+
+    [Range(0, 1000, ErrorMessage = "O estoque deve ser um valor entre 0 e 1000.")]
     public int Estoque { get; set; }
 }
